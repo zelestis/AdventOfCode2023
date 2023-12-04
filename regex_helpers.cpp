4d9a2ff884a6b69cad8ef93440693c9399ed5222
njvdmerwe <nick.jvdmerwe@ninemilefinancial.com>
2023-12-04 13:11:28 +1100

    aoc regex tests

--- regex_helpers.cpp
+++ regex_helpers.cpp
@@ -1,22 +1,55 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-vector<string> getMatches(const string &input, string reg_in) { // maybe put an lvalue ref on reg_in
-  vector<string> found_matches{};
-  regex re("(?=" + reg_in + ").");
-  smatch m;
+vector<string> getMatches(
+        const string &input, const string &reg_in, bool overlap = true
+) { // maybe put an lvalue ref on reg_in
+    vector<string> found_matches{};
+    regex re;
+    if (overlap) {
+        re = regex("(?=" + reg_in + ").");
+    } else {
+        re = regex(reg_in);
+    }
+    smatch m;
 
-  sregex_iterator iter(input.begin(), input.end(), re);
-  sregex_iterator end;
+    sregex_iterator iter(input.begin(), input.end(), re);
+    sregex_iterator end;
 
-  while (iter != end) {
-    found_matches.push_back(iter->str(1));
-    ++iter;
-  }
+    while (iter != end) {
+        if (overlap) {
+            found_matches.push_back(iter->str(1));
+        } else {
+            found_matches.push_back(iter->str());
+        }
+        ++iter;
+    }
 
-  return found_matches;
+    return found_matches;
 }
 // usage
 // auto matches = getMatches(tkn, "(one|two|three|four|five|six|seven|eight|nine|[0-9])");
 // might require you to remove the re line - that's to allow overlaps.
 // just replace it with re(reg_in)
+
+string regReplace(const string &input, const string &reg_in, const string &replace) {
+    regex reg(reg_in);
+    return regex_replace(input, reg, replace);
+}
+
+string addValueToMatch(const string &input, const string &regexPattern, const int valueToAdd) {
+    regex re(regexPattern);
+    sregex_iterator iter(input.begin(), input.end(), re);
+    sregex_iterator end;
+    string result = input;
+    while (iter != end) {
+        smatch match = *iter;
+        size_t matchPos = match.position();
+        for (size_t i = 0; i < match.length(); ++i) {
+            result[matchPos + i] += valueToAdd;
+        }
+        ++iter;
+    }
+    return result;
+}
---
+++ tests/test_regex_helpers.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include "../regex_helpers.cpp"
+using namespace std;
+
+string fix1 = "...1234.....#543.#223$......";
+string fix2 = "...TWONE...";
+
+void test_addValueToMatch() {
+    string test1 = fix1;
+    auto test1_out = addValueToMatch(test1, "[0-9]+", 'a'-'0');
+    if(test1_out != "...bcde.....#fed.#ccd$......") {
+        cout << "FAIL addValueToMatch" << endl;
+    } else {
+        cout << "PASS addValueToMatch" << endl;
+    }
+}
+
+void test_getMatches() {
+    string test2 = fix1;
+    auto test2_out = getMatches(test2, "[0-9]+", false);
+    if(stoi(test2_out[0]) != 1234) {
+        cout << "FAIL getMatches1" << endl;
+    }
+    else if(stoi(test2_out[1]) != 543) {
+        cout << "FAIL getMatches1" << endl;
+    }
+    else if(stoi(test2_out[2]) != 223) {
+        cout << "FAIL getMatches2" << endl;
+    }
+    else {
+        cout << "PASS getMatches_t1" << endl;
+    }
+}
+
+void test_getMatchesOverlap() {
+    string test3 = fix2;
+    auto test3_out = getMatches(test3, "(TWO|ONE)", true);
+    if(test3_out.size() != 2) {
+        cout << "FAIL getMatch_t2 size" << endl;
+    }
+    else if(test3_out[0] != "TWO") {
+        cout << "FAIL getMatch_t2 1" << endl;
+    }
+    else if(test3_out[1] != "ONE") {
+        cout << "FAIL getMatch_t2 2" << endl;
+    }
+    else {
+        cout << "PASS getMatch_t2" << endl;
+    }
+}
+
+void test_regexReplace() {
+    string test = fix1;
+    auto out = regReplace(test, "[0-9]", "A");
+    if(out != "...AAAA.....#AAA.#AAA$......") {
+        cout << "FAIL test_regexReplace" << endl;
+    } else {
+        cout << "PASS test_regexReplace" << endl;
+    }
+}
+
+void test_regexReplace_2() {
+    string test = fix1;
+    auto out = regReplace(test, "[0-9]+", "A");
+    if(out != "...A.....#A.#A$......") {
+        cout << "FAIL test_regexReplace_2" << endl;
+    } else {
+        cout << "PASS test_regexReplace_2" << endl;
+    }
+}
+
+int main() {
+    test_addValueToMatch();
+    test_getMatches();
+    test_getMatchesOverlap();
+    test_regexReplace();
+    test_regexReplace_2();
+}
