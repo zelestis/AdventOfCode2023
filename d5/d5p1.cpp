a19998f7982bedd5234e0820e642fb323a8dd3c2
njvdmerwe <nick.jvdmerwe@ninemilefinancial.com>
2023-12-05 17:50:55 +1100

    d5p1

--- d5/d5p1.cpp
+++ d5/d5p1.cpp
@@ -20,7 +20,8 @@ constexpr int MOD = 1000000007;
 constexpr ll INF2 = (ll)1 << 47;
 
 bool debug = false;
-
+constexpr int N = 150;
+string grid[N];
 
 int main() {
     if(!debug) {
@@ -29,5 +30,58 @@ int main() {
         cout.tie(0);
     }
 
+    printf("alive0\n");
+    for (auto & i : grid) getline(cin, i);
+
+    printf("alive1\n");
+    vi seeds{};
+    for(const string & v : getMatches(grid[0], "[0-9]+", false)) {
+        printf("stolld %s", v.c_str());
+        seeds.PB(stoll(v));
+    }
+    printf("alive 1\n");
+    int ind = 3;
+    printf("alive 2\n");
+    vector<map<ll, pii>> groups(7, map<ll, pii>{});
+    printf("alive 3\n");
+    REP(i, 0, 7) {
+        printf("begin %d\n", i);
+        auto & cmap = groups[i];
+        while(ind < N && grid[ind].length() > 4) {
+            printf("Parsing %s\n", grid[ind].c_str());
+            auto m = getMatches(grid[ind], "[0-9]+", false);
+            cmap[stoll(m[1])] = pii(stoll(m[0]), stoll(m[2]));
+            ind++;
+        }
+        ind += 2;
+    }
+    printf("alive 2\n");
+
+    ll lowestLocation = (ll)1 << 47;
+    for(auto & s : seeds) {
+        ll cval = s;
+        printf("Starting %lld\n", cval);
+        REP(i, 0, 7) {
+            auto it = groups[i].lower_bound(cval);
+            if(it == groups[i].end()) {
+                printf("Beyond the max, skipping\n");
+                continue;
+            }
+            if(it != groups[i].begin() && it->first != cval) {
+                it--;
+            }
+            ll lower = it->F;
+            ll upper = it->F + it->S.S;
+            printf("On {%lld [%lld %lld]} went from %lld to ", it->F, lower, upper, cval);
+            if(lower <= cval && cval < upper) {
+                cval = cval + (it->S.F  - it->F);
+            }
+            printf("to %lld\n", cval);
+        }
+        printf("Location = %lld\n", cval);
+        lowestLocation = min(cval, lowestLocation);
+    }
+
+    cout << lowestLocation << endl;
     return EXIT_SUCCESS;
 }
