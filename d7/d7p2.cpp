c457fc986d2a90da59679f612c034861ece6c669
njvdmerwe <nick.jvdmerwe@ninemilefinancial.com>
2023-12-08 14:29:21 +1100

    d7 done

--- d7/d7p2.cpp
+++ d7/d7p2.cpp
@@ -21,6 +21,51 @@ constexpr ll INF2 = (ll)1 << 47;
 
 bool debug = false;
 
+constexpr ll N = 1000;
+//constexpr ll N = 5;
+struct hand {
+    string c;
+    ll b, r, w;
+};
+
+int lu[256];
+hand hv[N];
+
+umap<char, ll> worth{
+        {'A' , 1}, {'K', 2}, {'Q', 3}, {'J', 100}, {'T', 5}
+};
+
+ll solve(const string &w) {
+    memset(lu, 0, sizeof(lu));
+    ll js = 0;
+    for (auto &c: w) if (c != 'J') lu[c]++;
+    for (auto &c: w) if (c == 'J') js++;
+    sort(begin(lu), end(lu), greater<int>{});
+    if (lu[0] + js > 5) printf("%lld", lu[0] + js);
+    if (lu[0] + js == 5) {
+        return 10;
+    }
+    if (lu[0] + js == 4) {
+        return 9;
+    }
+    // so now it's impossible for js >= 3
+    // if js == 2, only [1, 1] is possible. with 2 we would've made 4
+    // if js == 1, then [2, 2] -> [3, 2]
+    //                  [2, 1] -> [3, 1]
+    //                  [1, 1] -> [2, 1]
+    if (js == 2) return 7;
+    if (js == 1) {
+        if (lu[0] == 2 && lu[1] == 2) return 8;
+        if (lu[0] + 1 == 3) return 7;
+        return 5;
+    }
+
+    if (lu[0] == 3 && lu[1] == 2) return 8;
+    if (lu[0] == 3) return 7;
+    if (lu[0] == 2 && lu[1] == 2) return 6;
+    if (lu[0] == 2) return 5;
+    return 4;
+}
 
 int main() {
     if(!debug) {
@@ -28,6 +73,32 @@ int main() {
         cin.tie(0);
         cout.tie(0);
     }
+    REPe(i, 2, 9) {
+        printf("%d -> %d\n", i, 17-i);
+        worth[i+'0'] = 17-i;
+    }
+
+    REP(i, 0, N) {
+        cin >> hv[i].c >> hv[i].b;
+        hv[i].w = solve(hv[i].c);
+    }
+    sort(begin(hv), end(hv), [](hand & a, hand & b){
+        if(a.w == b.w) {
+            REP(i, 0, 5) {
+                if(a.c[i] == b.c[i]) continue;
+                return worth[a.c[i]] > worth[b.c[i]];
+            }
+        }
+        return a.w < b.w;
+    });
+
+    printf("Sorted\n");
+    ll tot = 0;
+    REP(i, 0,N) {
+        cout << hv[i].w << " = " << hv[i].c << " -> " << (i+1) << " * " << hv[i].b << endl;
+        tot += (i+1) * hv[i].b;
+    }
+    cout << "total = " << tot << endl;
 
     return EXIT_SUCCESS;
 }
